3c24539abfb4e320fb527773dc17baf0ae937bac
CerealKillerJohn <JWRhine@proton.me>
2024-02-05 10:01:50 -0500

    moved firefox to main browser

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -9,46 +9,46 @@
           extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
             bitwarden
             cookie-autodelete
-#            (enhancer-for-youtube.overrideAttrs {meta.license.free = true;})
-#            facebook-container #
+            (enhancer-for-youtube.overrideAttrs {meta.license.free = true;})
+            facebook-container #
             (grammarly.overrideAttrs {meta.license.free = true;}) #
-#            hulu-ad-blocker # doesn't exist yet
+            hulu-ad-blocker # doesn't exist yet
             (languagetool.overrideAttrs {meta.license.free = true;}) #
-#            (momentumdash.overrideAttrs {meta.license.free = true;})
+            (momentumdash.overrideAttrs {meta.license.free = true;})
             privacy-badger
-#            purple-ads-blocker # doesn't exist yet
-#            sponsorblock #
-#            steam-database
-#            to-google-translate # doesn't exist yet
-#            tridactyl #
+            purple-ads-blocker # doesn't exist yet
+            sponsorblock #
+            steam-database
+            to-google-translate # doesn't exist yet
+            tridactyl #
             ublock-origin
-#            unwanted-twitch # doesn't exist yet
-#            youtube-shorts-block #
+            unwanted-twitch # doesn't exist yet
+            youtube-shorts-block #
           ];
           settings = {
-#            "browser.startup.homepage" = "icloud.com";
-            "browser.startup.homepage" = "mail.proton.me";
+            "browser.startup.homepage" = "icloud.com";
+#            "browser.startup.homepage" = "mail.proton.me";
             "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
             "browser.tabs.warnOnClose" = false;
             "extensions.pocket.enabled" = false;
 #             "media.ffmpeg.vaapi.enabled" = true;
           };
-          userChrome = ''
-            @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
+#          userChrome = ''
+#            @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
 
             #navigator-toolbox {
-              height: 0px !important;
-              min-height: 0px !important;
-              overflow: hidden !important;
-            }
+#              height: 0px !important;
+#              min-height: 0px !important;
+#              overflow: hidden !important;
+#            }
 
             #navigator-toolbox:focus,
             #navigator-toolbox:focus-within,
             #navigator-toolbox:active {
-              height: auto !important;
-              overflow: visible !important;
-            }
-          '';
+#              height: auto !important;
+#              overflow: visible !important;
+#            }
+#          '';
         };
       };
     };
