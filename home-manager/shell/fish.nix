5b21364cafa39c0634bffe118ac89c2a5f6158a7
CerealKillerJohn <JWRhine@proton.me>
2024-02-05 23:28:22 -0500

    added wfetch to fish

--- home-manager/shell/fish.nix
+++ home-manager/shell/fish.nix
@@ -6,7 +6,8 @@
       enable = true;
       shellInit = ''
         set fish_greeting
-        nitch
+#        nitch
+        wfetch --challenge --challenge-timestamp 1676062449 --wallpaper --exit
         cat ~/.cache/wallust/sequences
       '';
     };
