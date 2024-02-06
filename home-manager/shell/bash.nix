1470cbb9bb16a81bdb5dcec26a9bc64e642a86ae
CerealKillerJohn <JWRhine@proton.me>
2024-02-06 00:02:52 -0500

    gnome challenge

--- home-manager/shell/bash.nix
+++ home-manager/shell/bash.nix
@@ -6,7 +6,7 @@
       enable = true;
       initExtra = ''
 #        nitch
-        wfetch --challenge --challenge-timestamp 1676062449 --wallpaper --exit
+        wfetch --challenge --challenge-timestamp 1676062449 --wallpaper
         cat ~/.cache/wallust/sequences
       '';
     };
