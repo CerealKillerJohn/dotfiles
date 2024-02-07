a1a509a8f98e8c54f7dd7c63625fe802682f93bb
CerealKillerJohn <JWRhine@proton.me>
2024-02-07 08:23:13 -0500

    moving few things from config to home

--- hosts/amd/configuration.nix
+++ hosts/amd/configuration.nix
@@ -69,11 +69,4 @@
       enable = true;
     };
   };
-
-  programs = {
-    fish = {
-      shellInit = " wfetch --challenge --challenge-type NIXOS --challenge-years 2 --challenge-timestamp 1676062449 --wallpaper";
-    };
-  };
-
 }
--- hosts/amd/home.nix
+++ hosts/amd/home.nix
@@ -3,4 +3,10 @@ _:
 {
   imports = [
   ];
+
+  programs = {
+    fish = {
+      shellInit = " wfetch --challenge --challenge-type NIXOS --challenge-years 2 --challenge-timestamp 1676062449 --wallpaper";
+    };
+  };
 }
