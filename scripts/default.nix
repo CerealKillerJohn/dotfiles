53766aa618110aef261ad169ed316c962562730a
CerealKillerJohn <JWRhine@proton.me>
2024-02-06 22:24:23 -0500

    working on wallpaper script

--- scripts/default.nix
+++ scripts/default.nix
@@ -2,22 +2,6 @@ _:
 
 {
   imports = [
-    ./audio-mic-mute
-    ./brightness-down
-    ./brightness-up
-    ./http_url_handler.py
-    ./install
-    ./kitty_opacity
-    ./powermenu.rasi
-    ./powermenu.sh
     ./random-wallpaper
-    ./rofi-power-menu
-    ./spotify.sh
-    ./sys-stats
-    ./time-date
-    ./vol-down
-    ./vol-mute
-    ./vol-up
-    ./weather
   ];
 }
