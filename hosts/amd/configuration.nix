4115b18fda99e968544315333d590067d4466070
CerealKillerJohn <JWRhine@proton.me>
2024-02-06 20:38:02 -0500

    fixed fish.nix

--- hosts/amd/configuration.nix
+++ hosts/amd/configuration.nix
@@ -72,7 +72,7 @@
 
   programs = {
     fish = {
-      shellInit = " wfetch --challenge --challenge-type NIXOS --challenge-years 2 --challenge-timestamp 1676062449 --wallpaper"
+      shellInit = " wfetch --challenge --challenge-type NIXOS --challenge-years 2 --challenge-timestamp 1676062449 --wallpaper";
     };
   };
 
