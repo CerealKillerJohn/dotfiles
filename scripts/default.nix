ab4270e54f7dcc82ec0db48c5a30a0ca7f8fcaae
CerealKillerJohn <JWRhine@proton.me>
2024-02-07 16:03:13 -0500

    more script work

--- scripts/default.nix
+++ scripts/default.nix
@@ -2,9 +2,14 @@ _:
 
 {
   imports = [
+    ./audio-mic-mute
+    ./powermenu.
     ./random-wallpaper
+    ./sys-stats
+    ./time-date
     ./vol-down
     ./vol-mute
     ./vol-up
+    ./weather
   ];
 }
