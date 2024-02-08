dcf5da6b951ecf25f2ffee349cb0190688f853d5
CerealKillerJohn <JWRhine@proton.me>
2024-02-08 08:55:07 -0500

    fixed home.nix on latitude

--- hosts/latitude/home.nix
+++ hosts/latitude/home.nix
@@ -6,7 +6,7 @@ _:
 
   programs = {
     fish = {
-      shellInit = "wfetch --challenge --challenge-type GNOME --challenge-years 0 --challenge-months 6 --challenge-timestamp 1701208432 --wallpaper"
+      shellInit = "wfetch --challenge --challenge-type GNOME --challenge-years 0 --challenge-months 6 --challenge-timestamp 1701208432 --wallpaper";
     };
   };
 }
