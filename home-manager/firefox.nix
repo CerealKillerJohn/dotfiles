f31f4b4660a553ef20e18833fa7d327b5f417be6
CerealKillerJohn <JWRhine@proton.me>
2024-02-08 07:43:12 -0500

    added webmail profile to firefox.nix

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -5,24 +5,24 @@
     firefox= {
       enable = true;
       profiles = {
-        ${user} = {
+        # ${user} = {
+        jwrhine = {
           extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
             bitwarden
             cookie-autodelete
             (enhancer-for-youtube.overrideAttrs {meta.license.free = true;})
             facebook-container #
             (grammarly.overrideAttrs {meta.license.free = true;}) #
-            hulu-ad-blocker # doesn't exist yet
+            hulu-ad-blocker
             (languagetool.overrideAttrs {meta.license.free = true;}) #
             (momentumdash.overrideAttrs {meta.license.free = true;})
             privacy-badger
-            purpleadblock # doesn't exist yet
-            sponsorblock #
+            purpleadblock
+            sponsorblock
             steam-database
-            to-google-translate # doesn't exist yet
-            tridactyl #
+            to-google-translate
             ublock-origin
-            unwanted-twitch # doesn't exist yet
+            unwanted-twitch
             youtube-shorts-block #
           ];
           settings = {
@@ -33,22 +33,39 @@
             "extensions.pocket.enabled" = false;
 #             "media.ffmpeg.vaapi.enabled" = true;
           };
-#          userChrome = ''
-#            @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
+        webmail = {
+          isDefault = false;
+          extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+            bitwarden
+            cookie-autodelete
+            (grammarly.overrideAttrs {meta.license.free = true;}) #
+            (languagetool.overrideAttrs {meta.license.free = true;}) #
+            privacy-badger
+            to-google-translate
+            ublock-origin
+          ];
+          settings = {
+            "browser.startup.homepage" = "mail.proton.me";
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            "browser.tabs.warnOnClose" = false;
+            "extensions.pocket.enabled" = false;
+          };
+          userChrome = ''
+            @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
 
             #navigator-toolbox {
-#              height: 0px !important;
-#              min-height: 0px !important;
-#              overflow: hidden !important;
-#            }
+              height: 0px !important;
+              min-height: 0px !important;
+              overflow: hidden !important;
+            }
 
             #navigator-toolbox:focus,
             #navigator-toolbox:focus-within,
             #navigator-toolbox:active {
-#              height: auto !important;
-#              overflow: visible !important;
-#            }
-#          '';
+              height: auto !important;
+              overflow: visible !important;
+            }
+          '';
         };
       };
     };
