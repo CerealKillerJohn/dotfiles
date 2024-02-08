e77dc88a9451857ae2e26b01d9c2d4b5d7e4de66
CerealKillerJohn <JWRhine@proton.me>
2024-02-08 08:51:38 -0500

    fixed home.nix on latitude

--- hosts/latitude/configuration.nix
+++ hosts/latitude/configuration.nix
@@ -55,11 +55,4 @@
       enable = true;
     };
   };
-
-  programs = {
-    fish = {
-      shellInit = " wfetch --challenge --challenge-type GNOME --challenge-years 0 --challenge-months 6 --challenge-timestamp 1701208432 --wallpaper"
-    };
-  };
-
 }
--- hosts/latitude/home.nix
+++ hosts/latitude/home.nix
@@ -3,4 +3,10 @@ _:
 {
   imports = [
   ];
+
+  programs = {
+    fish = {
+      shellInit = "wfetch --challenge --challenge-type GNOME --challenge-years 0 --challenge-months 6 --challenge-timestamp 1701208432 --wallpaper"
+    };
+  };
 }
